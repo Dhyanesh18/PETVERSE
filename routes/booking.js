13bf679eece7d03666371b975a907ed3066193a1
jeevanm27 <jeevan231227@gmail.com>
2025-05-04 22:59:43 +0530

    Create booking routes

---
+++ routes/booking.js
@@ -0,0 +1,10 @@
+const express = require('express');
+const router = express.Router();
+const bookingController = require('../controllers/bookingController');
+const { isAuthenticated } = require('../middleware/auth');
+
+router.get('/:serviceId', isAuthenticated, bookingController.getBookingPage);
+router.get('/available/slots', isAuthenticated, bookingController.getAvailableSlots);
+router.post('/', isAuthenticated, bookingController.bookSlot);
+
+module.exports = router;
