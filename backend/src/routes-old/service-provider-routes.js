30bcb3902d61f3a18583912c5035e8ec874c1335
Dhyanesh18 <dhyaneshvark1805@gmail.com>
2025-10-30 17:41:39 +0530

    userRoutes and mateRoutes converted to return json instead of rendering

--- backend/src/routes-old/service-provider-routes.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const { isAuthenticated, isServiceProvider } = require('../middleware/auth');
-const { getDashboard } = require('../controllers/serviceProviderController');
-
-router.get('/dashboard', isAuthenticated, isServiceProvider, getDashboard);
-
-module.exports = router; 
